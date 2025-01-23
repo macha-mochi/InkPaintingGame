b006728c59ec98c6aef5c2f0caf618417b355472
Faye-Yu <fayeyu0103@gmail.com>
2025-01-22 23:58:34 -0800

    added fungus + started copying over story

---
+++ Assets/StoryFX.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryFX : MonoBehaviour
+{
+    //this script is for all the various visual effects you're going to need...
+
+    [Header("Change BG To Teaset")]
+    [SerializeField] SpriteRenderer backgroundToChangeToTeaset;
+    [SerializeField] Sprite teasetBackground;
+    public void ChangeBackgroundToTeaset()
+    {
+        //backgroundToChangeToTeaset.sprite = teasetBackground;
+        Debug.Log("background changed, go back and uncomment real code later XD");
+    }
+}
